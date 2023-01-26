5fc7e61ea896474bedd275ee6791e3e625d80fd6
Ana Sabina Uban <ana.uban@gmail.com>
2023-01-26 15:05:11 +0200

    restore usage of additional custom wordlist

--- api/services/ocr_service.py
+++ api/services/ocr_service.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 from subprocess import run
+from nlp.resources.constants import WORDLIST_PATH
 
 LOGGER = logging.getLogger(__name__)
 
@@ -9,7 +10,8 @@ OCRMYPDF = "ocrmypdf"
 
 
 def call_ocr(in_file, pdf_output, txt_output):
-    cmd_args = CMD_ARGS + ["--sidecar", txt_output]
+    LOGGER.debug("Wordlist:" + WORDLIST_PATH)
+    cmd_args = CMD_ARGS + ["--sidecar", txt_output, "--user-words", WORDLIST_PATH]
     ocrmypdf_args = [OCRMYPDF, *cmd_args, in_file, pdf_output]
     proc = run(ocrmypdf_args, capture_output=True, encoding="utf-8")
     if proc.returncode != 0:
@@ -24,7 +26,7 @@ async def call_ocr_async(in_file, pdf_output, txt_output):
     # sub process is here, and here is where the exception happens.
     # TODO: when --sidecar is provided and the pdf already has text,
     # the cmd skips all pages with text
-    cmd_args = CMD_ARGS + ["--sidecar", txt_output]
+    cmd_args = CMD_ARGS + ["--sidecar", txt_output, "--user-words", WORDLIST_PATH]
     ocrmypdf_args = [OCRMYPDF, *cmd_args, in_file, pdf_output]
     proc = await asyncio.create_subprocess_exec(
         *ocrmypdf_args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
