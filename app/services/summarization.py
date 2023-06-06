4dd39dfedfb068d64725d1d6003ff690fc102313
snisioi <sergiu.nisioi@gmail.com>
2023-06-06 09:46:20 +0300

    1.1.4 speciffic summarization language

--- app/services/summarization.py
+++ app/services/summarization.py
@@ -7,8 +7,10 @@ from spacy.lang.ro import Romanian
 
 from app.config import SUMMARIZATION_METHOD, SUMMARY_PHRASES, SUMMARY_SENTENCES
 
+MAX_LEN = 2**18
 SNLP = Romanian()
 SNLP.add_pipe("sentencizer")
+SNLP.max_length = MAX_LEN  # to avoid memory issues
 logger = logging.getLogger(__name__)
 
 
@@ -17,7 +19,8 @@ def summarize(text: str) -> str:
     if SUMMARIZATION_METHOD not in SNLP.pipe_names:
         SNLP.add_pipe(SUMMARIZATION_METHOD, last=True)
     logger.info(f"Summarizing text of {len(text)} bytes...")
-    doc = SNLP(text)
+    # no need to do a summary of a very large text
+    doc = SNLP(text[:MAX_LEN])
     summary = list(
         doc._.textrank.summary(
             limit_phrases=SUMMARY_PHRASES, limit_sentences=SUMMARY_SENTENCES
